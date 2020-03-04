142421ba1488fe50d9a985832e3d938d28aa3f01
James Holman <james.holman@domain.com.au>
2020-03-04 21:52:27 +1100

    :alembic: login stored in session

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer, gql } from 'apollo-server-express';
+import * as session from 'express-session';
+import * as express from 'express';
 import * as fs from 'fs';
 import { createConnection } from 'typeorm';
 import resolvers from './resolvers';
@@ -7,25 +9,43 @@ import { entitiesContext, entities } from './entities';
 
 const typeDefs = gql(fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf8'));
 
-createConnection({
-  type: 'postgres',
-  host: 'db',
-  port: 5432,
-  username: 'jayylmao',
-  password: 'yeetus',
-  database: 'chatapp',
-  entities,
-  synchronize: true,
-  logging: false,
-}).then(async () => {
+const startServer = async () => {
+  await createConnection({
+    type: 'postgres',
+    host: 'db',
+    port: 5432,
+    username: 'jayylmao',
+    password: 'yeetus',
+    database: 'chatapp',
+    entities,
+    synchronize: true,
+    logging: false,
+  });
+
+  const app = express();
+
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: { ...entitiesContext },
+    context: ({ res, req }) => ({ req, res, data: entitiesContext }),
   });
 
-  server.listen().then(({ url }) => {
-    // eslint-disable-next-line no-console
-    console.log(`Server ready at ${url}`);
+  app.use(
+    session({
+      secret: 'afasgasd',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
+  server.applyMiddleware({
+    app,
   });
-});
+
+  app.listen({ port: 4000 }, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server ready at 4000`);
+  });
+};
+
+startServer();
--- api/src/resolvers/mutation-resolver.ts
+++ api/src/resolvers/mutation-resolver.ts
@@ -3,14 +3,14 @@ import { hash } from 'bcrypt';
 const SALT_ROUNDS = 10;
 
 export default {
-  createUser: async (_parent, { username, password }, { User }) => {
+  createUser: async (_parent, { username, password }, { data: { User } }) => {
     const user = new User();
     const hashedPass = await hash(password, SALT_ROUNDS);
     user.username = username;
     user.password = hashedPass;
     return user.save();
   },
-  createMessage: (_parent, args, { Message }) => {
+  createMessage: (_parent, args, { data: { Message } }) => {
     const message = new Message();
     message.text = args.text;
     message.authorId = args.authorId;
